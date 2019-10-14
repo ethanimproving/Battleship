ed0e387967ac8d2b54d39b3b0374be16a3e727ab
Ethan Miller <Ethan.Miller@improving.com>
2019-10-14 13:38:31 -0500

    initial commit

---
+++ src/main/java/org/improving/Main.java
@@ -0,0 +1,169 @@
+package org.improving;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static int numRows = 8;
+    public static int numCols = 8;
+    public static int computerShips;
+    public static List<Ship> ships = new ArrayList<>();
+    public static String[][] grid = new String[numRows][numCols];
+    public static int[][] missedGuesses = new int[numRows][numCols];
+
+    public static void main(String[] args){
+        System.out.println("**** Welcome to Battle Ships game ****");
+        System.out.println("Right now, sea is empty\n");
+
+        //Step 1 – Create the ocean map
+        createOceanMap();
+
+        //Step 2 - Deploy computer's ships
+        deployComputerShips();
+
+        //Step 3 Battle
+        do {
+            Battle();
+        }while(Main.computerShips != 0);
+
+        //Step 5 - Game over
+        gameOver();
+    }
+
+    public static void createOceanMap(){
+        //First section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+
+        //Middle section of Ocean Map
+        for(int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                grid[i][j] = " ";
+                if (j == 0)
+                    System.out.print(i + "|" + grid[i][j]);
+                else if (j == grid[i].length - 1)
+                    System.out.print(grid[i][j] + "|" + i);
+                else
+                    System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+
+        //Last section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+
+    public static void deployComputerShips(){
+        System.out.println("\nComputer is deploying ships");
+
+        //Create ships
+        var five = new Ship(5);
+        var four = new Ship(4);
+        var three = new Ship(3);
+        var two = new Ship(2);
+
+        Main.ships.add(five);
+        Main.ships.add(four);
+        Main.ships.add(three);
+        Main.ships.add(two);
+
+        //Deploying five ships for computer
+        Main.computerShips = 5;
+        for (int i = 1; i <= Main.computerShips; ) {
+            int x = (int)(Math.random() * 10);
+            int y = (int)(Math.random() * 10);
+
+            if((x >= 0 && x < numRows) && (y >= 0 && y < numCols) && (grid[x][y] == " "))
+            {
+                grid[x][y] =   "x";
+                System.out.println(i + ". ship DEPLOYED");
+                i++;
+            }
+        }
+        printOceanMap();
+    }
+
+    public static void Battle(){
+        playerTurn();
+
+        printOceanMap();
+
+        System.out.println();
+        System.out.println("Computer ships: " + Main.computerShips);
+        System.out.println();
+    }
+
+    public static void playerTurn(){
+        System.out.println("\nYOUR TURN");
+        int x = -1, y = -1;
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter X coordinate: ");
+            x = input.nextInt();
+            System.out.print("Enter Y coordinate: ");
+            y = input.nextInt();
+
+            if ((x >= 0 && x < numRows) && (y >= 0 && y < numCols)) //valid guess
+            {
+                if (grid[x][y] == "x") //if computer ship is already there; computer loses ship
+                {
+                    System.out.println("Boom! You sunk the ship!");
+                    grid[x][y] = "!"; //Hit mark
+                    --Main.computerShips;
+                }
+                else if (grid[x][y] == " ") {
+                    System.out.println("Sorry, you missed");
+                    grid[x][y] = "-";
+                }
+            }
+            else if ((x < 0 || x >= numRows) || (y < 0 || y >= numCols))  //invalid guess
+                System.out.println("You can't place ships outside the " + numRows + " by " + numCols + " grid");
+        }while((x < 0 || x >= numRows) || (y < 0 || y >= numCols));  //keep re-prompting till valid guess
+    }
+
+
+    public static void gameOver(){
+        System.out.println("Computer ships: " + Main.computerShips);
+        if(Main.computerShips <= 0)
+            System.out.println("Hooray! You won the battle :)");
+        else
+            System.out.println("Sorry, you lost the battle");
+        System.out.println();
+    }
+
+    public static void printOceanMap(){
+        System.out.println();
+        //First section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+
+        //int to letter
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+
+        //Middle section of Ocean Map
+        for(int x = 0; x < grid.length; x++) {
+            System.out.print(alphabet[x] + "|");
+
+            for (int y = 0; y < grid[x].length; y++){
+                System.out.print(grid[x][y]);
+            }
+
+            System.out.println("|" + alphabet[x]);
+        }
+
+        //Last section of Ocean Map
+        System.out.print("  ");
+        for(int i = 0; i < numCols; i++)
+            System.out.print(i);
+        System.out.println();
+    }
+}
---
+++ src/main/java/org/improving/Ship.java
@@ -0,0 +1,11 @@
+package org.improving;
+
+public class Ship {
+    int height;
+    int length;
+
+    public Ship(int length) {
+        this.height = 1;
+        this.length = length;
+    }
+}
